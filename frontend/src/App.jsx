43e1ba171da839dad47eb718fa39917941c42274
paramveer <paramveer@touchnote.in>
2023-09-19 19:22:48 +0530

    Second Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return <div className="bg-red-400 text-white">App</div>;
+  return (
+    <div className="bg-red-400 text-white w-100 m-10 h-40  p-5 rounded-lg text-center shadow-2xl text-2xl font-extrabold">
+      My Book Store App
+    </div>
+  );
 };
 
 export default App;
